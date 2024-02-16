27324187ba678c6188d6974a4301c4eadcf55836
Usuario <Usuario@Asus>
2024-02-16 11:21:46 +0100

    commint rama1

---
+++ Proyecto2/src/paqueteRama1/claseRama1.java
@@ -0,0 +1,10 @@
+package paqueteRama1;
+
+public class claseRama1 {
+	public static void main(String[] args) {
+		System.out.print("mensaje 1 de la rama1: ");
+	
+		System.out.print("mensaje 2  de la rama1 ");
+		
+	}
+}
